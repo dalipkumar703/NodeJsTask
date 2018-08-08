134eb0c93480c9ee7117dbc16c794937ee9cf713
dalipkumar703 <nitinthakkar@Nitins-MacBook-Air.local>
2018-08-08 21:19:38 +0530

    Test Auth

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,39 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _winston = require('winston');
+
+var _winston2 = _interopRequireDefault(_winston);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _jsonwebtoken = require('jsonwebtoken');
+
+var _jsonwebtoken2 = _interopRequireDefault(_jsonwebtoken);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
+
+app.use(_bodyParser2.default.json());
+app.use(_bodyParser2.default.urlencoded({
+  extended: true
+}));
+app.use(_express2.default.static('./'));
 app.get('/', function (req, res) {
   res.status(200).json({ "message": "Welcome to Node.js & Express" });
 });
+app.post('/login', function (req, res) {
+  authenticate(req, res);
+});
+var logger = _winston2.default.createLogger({
+  level: 'info',
+  format: _winston2.default.format.json(),
+  transports: [new _winston2.default.transports.File({ filename: 'error.log', level: 'error' }), new _winston2.default.transports.File({ filename: 'combined.log' })]
+});
+
 app.listen(process.env.PORT || 3000, function () {
-  return console.log("Listening to port 3000");
+  return logger.info("Listening to port 3000");
 });
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,16 +1,30 @@
-import dotenv from 'dotenv';
-import jwt from 'jsonwebtoken';
-const authenticate(req,res)={
-  var message;
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import {logger} from "./logger";
+dotenv.load();
+logger.info(process.env.token,"token value");
+const authenticate=(req,res)=>{
+  var reply={
+    message:""
+  };
   if(process.env.clientName!=req.body.name)
   {
-    message="wrong name";
+    reply.message="Wrong name";
   }
   if(process.env.password!=req.body.password)
   {
-    message+=" wrong password";
+    reply.message+=" Wrong password";
   }
-  else if(process.env.clientName===req.body.name&&process.env.password===req.body.password) {
-  var token  =jwt.sign(process.env.clientName,process.env.secret);
+  else if(process.env.clientName===req.body.name&&process.env.secret===req.body.password) {
+  reply.message="Login Successfully!";
+  var token=jwt.sign({name:process.env.clientName,password:process.env.secret},process.env.token);
+  }
+  if(token)
+  {
+  return  res.send(token);
+  }
+  else {
+  return  res.send(reply.message);
   }
 }
+export {authenticate};
