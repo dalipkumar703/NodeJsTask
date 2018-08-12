ed04dbec200cf7c57c20827cddfe4711478f5447
dalipkumar703 <nitinthakkar@Nitins-MacBook-Air.local>
2018-08-12 13:08:37 +0530

    logger remove path

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -2,11 +2,7 @@ import bunyan from "bunyan";
 /** Logger for creating log file
 * @log
 */
-const log = bunyan.createLogger({name: "NodeJsTask",
-streams: [{
-        path: './logs/nodetask.log',
-        // `type: 'file'` is implied
-    }]
+const log = bunyan.createLogger({name: "NodeJsTask"
 });
 export  {
 	log
